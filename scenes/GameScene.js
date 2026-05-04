438c12cd2e55d24acdb3a6f0ddcb38980bde069c
AndoRAZAFY <ando@natixgroup.com>
2026-05-04 19:19:22 +0300

    feat(game): add dust animation for player movement

--- scenes/GameScene.js
+++ scenes/GameScene.js
@@ -25,19 +25,44 @@ export default class GameScene extends Scene {
         this.lia = this.maki.player('ash')
         manager.map(this, 'default_map')
         manager.preload(this)
+
+        this.load.spritesheet('dust', 'Tiny Swords (Free Pack)/Particle FX/Dust_02.png', {
+            frameWidth: 64,
+            frameHeight: 64
+        })
     }
 
     create() {
         super.create()
         manager.create(this)
 
-        // Place lia in the center of the map (50×50 tiles × 16px = 800×800)
         this.lia.sprite.setPosition(400, 400)
 
         this.physics.add.collider(this.lia.sprite, manager.getWallGroup(this, 'default_map'))
+
+        this.anims.create({
+            key: 'dust-anim',
+            frames: this.anims.generateFrameNumbers('dust', { start: 0, end: 9 }),
+            frameRate: 12,
+            repeat: 0
+        })
+    }
+
+    _playDust(x, y) {
+        const fx = this.add.sprite(x, y, 'dust').setDepth(2)
+        fx.play('dust-anim')
+        fx.on('animationcomplete', () => fx.destroy())
     }
 
     update() {
+        const { x, y } = this.lia.sprite.body.velocity
+        const moving = x !== 0 || y !== 0
+
+        if (moving && !this._wasMoving) {
+            this._playDust(this.lia.sprite.x, this.lia.sprite.y + 20)
+        }
+        this._wasMoving = moving
+
         this.maki.move(this.lia)
     }
 }
