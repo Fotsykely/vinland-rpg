123eb93ea5916d9ec9411a0db6e11a0f1c498cc2
AndoRAZAFY <ando@natixgroup.com>
2026-05-04 19:09:10 +0300

    feat(assets): add new unit sprites and animations for Red and Yellow units feat(scenes): update GameScene to use new player sprite configuration

--- scenes/GameScene.js
+++ scenes/GameScene.js
@@ -1,9 +1,28 @@
 import { Scene, manager } from '@tialops/maki'
 
 export default class GameScene extends Scene {
+    _getConfig() {
+        return {
+            sprite: {
+                file: 'ash.png',
+                layout: 'row',
+                cols: 24,
+                rows: 1,
+                frameWidth: 32,
+                frameHeight: 64,
+                directions: {
+                    right: { start: 0, end: 5 },
+                    up: { start: 6, end: 11 },
+                    left: { start: 12, end: 17 },
+                    down: { start: 18, end: 23 }
+                }
+            }
+        }
+    }
+
     preload() {
         super.preload()
-        this.lia = this.maki.player('lia')
+        this.lia = this.maki.player('ash')
         manager.map(this, 'default_map')
         manager.preload(this)
     }
